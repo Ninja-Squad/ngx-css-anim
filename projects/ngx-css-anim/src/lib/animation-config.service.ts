15f9429ff71bb15c7cfdc612de0d94eda63c7595
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-08-21 17:16:45 +0200

    doc: document the animation config service

--- projects/ngx-css-anim/src/lib/animation-config.service.ts
+++ projects/ngx-css-anim/src/lib/animation-config.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * Service allowing to globally configure the behavior of the animate directive.
+ * It's typically used in tests: by injecting this service and setting the `animationsDisabled`
+ * flag to `true`, it makes all the animations triggered by the animate directive synchronous.
+ * If you call the animate function directly, without using the directive, it's a good idea to honor
+ * the `animationsDisabled` flag of this service, too.
+ */
 @Injectable({
   providedIn: 'root'
 })
